f0953fb30a0f94a38a1282ce37bee155f328f059
Ujjayanta <aprajit09041996@gmail.com>
2021-07-05 18:47:20 +0530

    Update asciiGan.py

--- asciiGan.py
+++ asciiGan.py
@@ -2,7 +2,7 @@ import streamlit as st
 from numpy import load
 from numpy import expand_dims
 from matplotlib import pyplot
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
 
